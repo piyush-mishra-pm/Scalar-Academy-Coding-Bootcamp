9dd40c75c1916cec25e14dec3732303b1eb7ec8e
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-02-05 23:22:51 +0530

    Scalar Academy DS-Algo repo started, commiting a BST question.

---
+++ DS Algo/BinarySearchTrees/Assignments/SortedArrayToBalancedBST/SortedArrayToBalancedBST.java
@@ -0,0 +1,20 @@
+public class SortedArrayToBalancedBST {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public TreeNode sortedArrayToBST(final int[] A) {
+        return solve(A,0,A.length-1);
+    }
+
+    private TreeNode solve(final int[] A, int start, int end){
+        if(start>end){
+            return null;
+        }
+
+        int mid = start+(end-start)/2;
+        TreeNode root = new TreeNode(A[mid]);
+
+        root.left = solve(A,start,mid-1);
+        root.right = solve(A,mid+1,end);
+
+        return root;
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+#Scalar Academy
+
+Learnings from Scalar Academy's coding bootcamp I attend.
+
+##Quick Links
+
+###Data Structure & Algorithm:
+
+- Stacks
+- Queues
+- Linked Lists
+- Binary Search Trees
+- Dynamic Programming
+- Graph
